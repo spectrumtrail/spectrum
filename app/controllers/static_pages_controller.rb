5a356a81a31bd4132242af6f2997fea9e5afb899
ColinW520 <colinhagan@gmail.com>
2019-01-11 17:17:50 -0600

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/terms'
+  get 'static_pages/privacy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
