610381f6cda131668998f46df33890049d8eeb82
ColinW520 <colinhagan@gmail.com>
2019-01-11 17:21:54 -0600

    implement HAML as the html pre-processor

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/terms'
-  get 'static_pages/privacy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "static_pages#home"
+
+  get "static_pages/home"
+  get "static_pages/terms"
+  get "static_pages/privacy"
 end
