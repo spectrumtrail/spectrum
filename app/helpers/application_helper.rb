bd91a28e074888197cb7932921f143ee2e0cbf3a
ColinW520 <colinhagan@gmail.com>
2019-01-14 11:43:41 -0600

    extends helper usability

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
-  def active_class(link_path)
-    current_page?(link_path) ? "active" : ""
+  def active_by_path(link_path)
+    current_page?(link_path) ? 'active' : ""
+  end
+
+  def active_by_text(text)
+    if request.path_info.include?(text)
+      "active"
+    end
   end
 end
