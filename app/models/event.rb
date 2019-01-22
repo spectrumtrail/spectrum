046b8066da060d245f94beb2c58f10c453b46b4f
ColinW520 <colinhagan@gmail.com>
2019-01-21 22:19:20 -0600

    ensure slugs regenerate as expected

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,9 @@
 class Event < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
+  def should_generate_new_friendly_id?
+    slug.blank? || name_changed?
+  end
 
   belongs_to :location
   has_many :races, dependent: :destroy
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,9 @@
 class Location < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
+  def should_generate_new_friendly_id?
+    slug.blank? || name_changed?
+  end
 
   has_many :events
   has_many :races, through: :events
