8c2363faa6d01240051ebb4af616618254f5429e
ColinW520 <colinhagan@gmail.com>
2019-01-23 07:49:12 -0600

    Remove froala looping

--- app/assets/javascripts/theme.js
+++ app/assets/javascripts/theme.js
@@ -101,19 +101,15 @@ $(document).ready(function() {
 
 
     // Froala Editor
-    $('textarea.editor').each(function() {
-      $(function() {
-        $(this).froalaEditor({
-          key: 'YE4G3G4A3c1A6B5B5E2A3B2D2G3C4C3D-17A-13yohxJ-7B-22bB1ef1rB-8yB1C-21B-16kupiA-8noC7bmn==',
-          iconsTemplate: 'font_awesome_5',
-          imageStyles: {
-            class1: 'img',
-            class2: 'img-fluid'
-          },
-        })
-      })
-    });
 
+    $("textarea.editor").froalaEditor({
+      key: "YE4G3G4A3c1A6B5B5E2A3B2D2G3C4C3D-17A-13yohxJ-7B-22bB1ef1rB-8yB1C-21B-16kupiA-8noC7bmn==",
+      iconsTemplate: "font_awesome_5",
+      imageStyles: {
+        class1: "img",
+        class2: "img-fluid"
+      },
+    })
 
     // NoUI Slider
     if ($(".input-slider-container")[0]) {
