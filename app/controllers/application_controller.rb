85ce5fda84fa90cfee6e3ca0273088a722ef7b1c
ColinW520 <colinhagan@gmail.com>
2019-01-23 08:29:22 -0600

    set time zone to central until we can lean on user defined time zones

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :danger, :info
+  around_action :set_time_zone
 
+  private
+
+  # this will be replaced when we generate a users table
+  def set_time_zone(&block)
+    Time.use_zone("Central Time (US & Canada)", &block)
+  end
 end
