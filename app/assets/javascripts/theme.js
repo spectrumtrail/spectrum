965757470c7db84b61de4a1327fd5828386f6f12
ColinW520 <colinhagan@gmail.com>
2019-01-29 11:55:48 -0600

    testing spotlight class on prod with parallax class

--- app/assets/javascripts/theme.js
+++ app/assets/javascripts/theme.js
@@ -19,36 +19,35 @@ $(window).on("load", function() {
 
 $(window).on('load resize', function() {
 
-    // Background image holder - Static hero with fullscreen autosize
-    if ($('.spotlight').length) {
-        $('.spotlight').each(function() {
+  // Background image holder - Static hero with fullscreen autosize
+  if ($('.spotlight').length) {
+    $('.spotlight').each(function() {
+      var $this = $(this);
+      var holderHeight;
 
-            var $this = $(this);
-            var holderHeight;
-
-            if ($this.data('spotlight') == 'fullscreen') {
-                if ($this.data('spotlight-offset')) {
-                    var offsetHeight = $('body').find($this.data('spotlight-offset')).height();
-                    holderHeight = $(window).height() - offsetHeight;
-                }
-                else {
-                    holderHeight = $(window).height();
-                }
+      if ($this.data('spotlight') == 'fullscreen') {
+        if ($this.data('spotlight-offset')) {
+          var offsetHeight = $('body').find($this.data('spotlight-offset')).height();
+          holderHeight = $(window).height() - offsetHeight;
+        }
+        else {
+          holderHeight = $(window).height();
+        }
 
 
-                if ($(window).width() > 991) {
-                    $this.find('.spotlight-holder').css({
-                        'height': holderHeight + 'px'
-                    });
-                }
-                else {
-                    $this.find('.spotlight-holder').css({
-                        'height': 'auto'
-                    });
-                }
-            }
-        })
-    }
+        if ($(window).width() > 991) {
+          $this.find('.spotlight-holder').css({
+              'height': holderHeight + 'px'
+          });
+        }
+        else {
+          $this.find('.spotlight-holder').css({
+              'height': 'auto'
+          });
+        }
+      }
+    })
+  }
 }),
 
 $(document).ready(function() {
