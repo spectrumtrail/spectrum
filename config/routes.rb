0c2d8ecd1999bcd0560e3b61238fe04bd427303c
ColinW520 <colinhagan@gmail.com>
2019-02-17 16:05:26 -0600

    adds a very static danger page

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,12 @@ Rails.application.routes.draw do
   match '/terms' => "static_pages#terms", via: [:get]
   match '/privacy' => "static_pages#privacy", via: [:get]
   match '/about' => "static_pages#about", via: [:get]
+  match '/danger' => "static_pages#danger", via: [:get]
 
   resources :attachments, only: [:destroy]
   resources :locations, only: [:index, :show]
   resources :events, only: [:index, :show] do
-    resources :registrations do
+    resources :registrations, only: [:new, :create, :edit, :update] do
       resources :steps, controller: "registration_steps"
     end
     resources :races
