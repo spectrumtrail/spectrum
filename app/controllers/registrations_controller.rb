fefda68487affa4975cacb292817d61d34ee3b0d
ColinW520 <colinhagan@gmail.com>
2019-02-17 16:15:31 -0600

    adds token checking to the registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,11 @@
 class RegistrationsController < ApplicationController
   before_action :set_event
-  before_action :set_registration, only: [:show, :edit, :update, :destroy]
+  before_action :set_registration, only: [:edit, :update]
+  before_action :check_token, only: [:edit, :update]
 
   def new
-    @registration = @event.registrations.new(started_at: Time.now, steps_seen: ["start"])
-    @participant = Participant.new(registration: @registration, race_id: params[:race_id])
+    @registration = build_new_registration
+    @participant = build_new_participant_for(@registration)
   end
 
   def create
@@ -12,15 +13,11 @@ class RegistrationsController < ApplicationController
     if @registration.save
       @registration.update(steps_completed: ["start"])
       redirect_to(
-        # takes you to the registration steps wizard Details step
         event_registration_steps_path(@event, @registration)
       )
     else
       render :new
     end
-  end
-
-  def show
   end
 
   def edit
@@ -37,17 +34,28 @@ class RegistrationsController < ApplicationController
     end
   end
 
-  def destroy
-    redirect_to(
-      root_path,
-      danger: "Registrations cannot be destroyed. This attempt has been logged."
-    )
+  private
+
+  def build_new_registration
+    @event.registrations.new(started_at: Time.now, steps_seen: ["start"])
   end
 
-  private
+  def build_new_participant_for(registration)
+    @event.participants.new(registration: registration, race_id: params[:race_id])
+  end
+
+  def check_token
+    if @registration.token != registration_token
+      redirect_to danger_path
+    end
+  end
 
   def registration_params
     params.require(:registration).permit!
+  end
+
+  def registration_token
+    params[:id].split("-").last
   end
 
   def set_event
