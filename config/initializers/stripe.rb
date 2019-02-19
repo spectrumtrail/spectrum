923b15426719f581410b31aae9579e0285684d3e
ColinW520 <colinhagan@gmail.com>
2019-02-19 10:18:46 -0600

    appease the test env

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,8 +3,8 @@ require "stripe"
 credentials_hash = Rails.application.credentials.send(Rails.env.to_sym)
 
 Rails.configuration.stripe = {
-  publishable_key: credentials_hash.fetch(:stripe_publishable_key),
-  secret_key: credentials_hash.fetch(:stripe_secret_key)
+  publishable_key: credentials_hash.fetch(:stripe_publishable_key, ""),
+  secret_key: credentials_hash.fetch(:stripe_secret_key, "")
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
