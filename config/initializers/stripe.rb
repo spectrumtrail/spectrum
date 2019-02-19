c80e8afbc37627650a3addaf21603ae00724bc0f
ColinW520 <colinhagan@gmail.com>
2019-02-19 10:24:46 -0600

    passes a default dummy key

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,8 +3,8 @@ require "stripe"
 credentials_hash = Rails.application.credentials.send(Rails.env.to_sym)
 
 Rails.configuration.stripe = {
-  publishable_key: credentials_hash.fetch(:stripe_publishable_key, ""),
-  secret_key: credentials_hash.fetch(:stripe_secret_key, "")
+  publishable_key: credentials_hash.fetch(:stripe_publishable_key, "test"),
+  secret_key: credentials_hash.fetch(:stripe_secret_key, "test")
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
