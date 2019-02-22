0c955d9d200083a84d1f1d8f3ea95fd528195a1d
ColinW520 <colinhagan@gmail.com>
2019-02-22 10:20:11 -0600

    order events alphaebetically

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -2,7 +2,7 @@ class Admin::EventsController < Admin::BaseController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   def index
-    @events = Event.all
+    @events = Event.order(:name)
   end
 
   def show
