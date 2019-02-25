a0247e1bf08306da84e546195900513e3cfa4566
ColinW520 <colinhagan@gmail.com>
2019-02-25 11:57:23 -0600

    require admins in Admin controllers

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,14 @@
 class Admin::BaseController < ApplicationController
+  before_action :require_admin!
   layout -> (controller) { controller.request.xhr? ? false : "admin/base" }
+
+  private
+
+  def require_admin!
+    unless current_user && current_user.is_admin?
+      flash[:alert] = "Sorry, only Spectrum Admin users can go here."
+      redirect_to root_path
+    end
+  end
+  helper_method :require_admin!
 end
