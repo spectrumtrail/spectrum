c642a3f294a9e3a6dd7b8d4fdd461978e28cdff5
ColinW520 <colinhagan@gmail.com>
2019-02-25 11:58:55 -0600

    enforce unique emails per event

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -11,6 +11,10 @@ class Participant < ApplicationRecord
     format: {
       with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i,
       message: "looks unusual. Please use a different one."
+    },
+    uniqueness: {
+      scope: [:race_id, :event_id],
+      message: "is already associated with a participant for this race. Have you already registered? If so, check your email for your confirmation. If you are trying to register someone else, please be sure to use their email address, and make sure they are with you so that they can accept the waiver terms on their own (legal stuff). This is important so that we know who is racing and how to get ahold of them."
     }
 
   validates :phone, presence: true, if: :validate_details?
