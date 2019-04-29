c35fb9e13b6f525ea0942d495b755dd75143158b
ColinW520 <colinhagan@gmail.com>
2019-04-29 10:31:37 -0500

    apply logic to credentials hash setting

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,10 @@
 require "stripe"
 
-credentials_hash = Rails.application.credentials.send(Rails.env.to_sym)
+if Rails.application.credentials.present?
+  credentials_hash = Rails.application.credentials.send(Rails.env.to_sym)
+else
+  credentials_hash = {}
+end
 
 Rails.configuration.stripe = {
   publishable_key: credentials_hash.fetch(:stripe_publishable_key, "test"),
