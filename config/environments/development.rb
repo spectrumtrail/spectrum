dee12407e5db38132991ddea23a44ba1b96fb242
ColinW520 <colinhagan@gmail.com>
2019-05-31 17:21:17 -0500

    feature: show emails to be delievered in dev mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,12 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { host: "localhost", port: 3004 }
+  config.action_mailer.default_url_options = { host: "localhost", port: 3008 }
   config.action_mailer.preview_path = "#{Rails.root}/lib/mailer_previews"
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
@@ -32,11 +36,6 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
-
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
