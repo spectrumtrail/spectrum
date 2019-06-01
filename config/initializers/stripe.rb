17e52ae9845c15264089745f0e2edeb49969f3bb
ColinW520 <colinhagan@gmail.com>
2019-06-01 11:59:45 -0500

    bugfix: fix stripe initializer
     - note: there may be an actual bug with Rails 5 credentials. 
    `Rails.application.credentials.present?` should not return false when an 
    object is returned. Need to confirm this.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,14 +1,16 @@
 require "stripe"
 
-if Rails.application.credentials.present?
-  credentials_hash = Rails.application.credentials.send(Rails.env.to_sym)
-else
-  credentials_hash = {}
-end
+credentials_hash = Rails.application.credentials.send(Rails.env.to_sym)
 
 Rails.configuration.stripe = {
-  publishable_key: credentials_hash.fetch(:stripe_publishable_key, "test"),
-  secret_key: credentials_hash.fetch(:stripe_secret_key, "test")
+  publishable_key: credentials_hash.fetch(
+    :stripe_publishable_key,
+    ENV["STRIPE_PUBLISHABLE_KEY"]
+  ),
+  secret_key: credentials_hash.fetch(
+    :stripe_secret_key,
+    ENV["STRIPE_SECRET_KEY"]
+  )
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
