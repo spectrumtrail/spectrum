c7a4603c7c9c132a4d633ba3f894cd980ada2232
ColinW520 <colinhagan@gmail.com>
2019-07-01 13:07:21 -0500

    refactor: simplify initializer to just use env vars

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,16 +1,8 @@
 require "stripe"
 
-credentials_hash = Rails.application.credentials.send(Rails.env.to_sym)
-
 Rails.configuration.stripe = {
-  publishable_key: credentials_hash.fetch(
-    :stripe_publishable_key,
-    ENV["STRIPE_PUBLISHABLE_KEY"]
-  ),
-  secret_key: credentials_hash.fetch(
-    :stripe_secret_key,
-    ENV["STRIPE_SECRET_KEY"]
-  )
+  publishable_key: ENV["STRIPE_PUBLIC_KEY"]
+  secret_key: ENV["STRIPE_SECRET_KEY"]
 }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
