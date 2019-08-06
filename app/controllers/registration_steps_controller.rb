8ce6872c1a8239ace593ba0b5c6e296212039631
ColinW520 <colinhagan@gmail.com>
2019-08-06 13:10:05 -0600

    bugfix: no cap or time check once payment complete

--- app/controllers/registration_steps_controller.rb
+++ app/controllers/registration_steps_controller.rb
@@ -35,20 +35,28 @@ class RegistrationStepsController < ApplicationController
   end
 
   def check_registration_window
-    unless @event.registration_open?(Time.current) # giving an hour to finish once started
-      redirect_to(
-        event_path(@event),
-        alert: "The time window to register for this event has closed before you could finish your registration. Please try again next year! If you're seeing this message, rest assured your credit card has NOT been charged."
-      ) and return
+    if step == :confirmation
+      true
+    else
+      unless @event.registration_open?(Time.current) # giving an hour to finish once started
+        redirect_to(
+          event_path(@event),
+          alert: "The time window to register for this event has closed before you could finish your registration. Please try again next year! If you're seeing this message, rest assured your credit card has NOT been charged."
+        ) and return
+      end
     end
   end
 
   def check_participant_count
-    unless @registration.race.is_registerable?(Time.current)
-      redirect_to(
-        event_path(@event),
-        alert: "This race is no longer available for registration. It has either been cancelled; or, has filled up. Please pick a different race, or, try again next year! If you're seeing this message, rest assured your credit card has NOT been charged."
-      ) and return
+    if step == :confirmation
+      true
+    else
+      unless @registration.race.is_registerable?(Time.current)
+        redirect_to(
+          event_path(@event),
+          alert: "This race is no longer available for registration. It has either been cancelled; or, has filled up. Please pick a different race, or, try again next year! If you're seeing this message, rest assured your credit card has NOT been charged."
+        ) and return
+      end
     end
   end
 
