033c872bc8ab8fdc9afd56295266f5068547b0f8
ColinW520 <colinhagan@gmail.com>
2019-08-21 11:28:51 -0600

    bugfix: use public_send to avoid kernel private method

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,11 +1,11 @@
 require "stripe"
 
 Rails.configuration.stripe = {
-  publishable_key: Rails.application.credentials.send(Rails.env.to_sym).fetch(
+  publishable_key: Rails.application.credentials.public_send(Rails.env.to_sym).fetch(
     :stripe_public_key,
     ENV["STRIPE_PUBLIC_KEY"]
   ),
-  secret_key: Rails.application.credentials.send(Rails.env.to_sym).fetch(
+  secret_key: Rails.application.credentials.public_send(Rails.env.to_sym).fetch(
     :stripe_secret_key,
     ENV["STRIPE_SECRET_KEY"]
   )
