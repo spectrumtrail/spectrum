74a18c5527173c57026ee61688e645e9ad55a722
ColinW520 <colinhagan@gmail.com>
2019-08-24 15:54:26 -0600

    refactor: clean up admin layout for team members

--- app/decorators/team_member_decorator.rb
+++ app/decorators/team_member_decorator.rb
@@ -1,3 +1,9 @@
 class TeamMemberDecorator < ApplicationDecorator
-
+  def row_thumbnail
+    if team_member.profile_image.attached?
+      helpers.url_for(team_member.profile_image.variant(resize: "100x100"))
+    else
+      helpers.asset_path("logo-no-text")
+    end
+  end
 end
