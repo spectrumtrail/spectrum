b9713f710482a03fc244d665db4eb328f6707b5f
William Blake <gwilliamblake@gmail.com>
2019-10-29 19:17:27 -0500

    feature: front-end changes for team members

--- app/controllers/team_members_controller.rb
+++ app/controllers/team_members_controller.rb
@@ -2,5 +2,7 @@ class TeamMembersController < ApplicationController
 
   def index
     @team_members = TeamMember.order(name: :asc)
+    @leaders = @team_members.select { |member| member.is_leader == true }
+    @blazers = @team_members.select { |member| member.is_leader == false }
   end
 end
