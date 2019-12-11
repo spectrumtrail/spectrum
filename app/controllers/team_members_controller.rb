2f813c33a06713db01f12b515497dddf2c23f161
ColinW520 <colinhagan@gmail.com>
2019-12-11 09:49:23 -0700

    chore: remove n+1 query on TeamMembers controller

--- app/controllers/team_members_controller.rb
+++ app/controllers/team_members_controller.rb
@@ -1,11 +1,17 @@
 class TeamMembersController < ApplicationController
   def index
-    @team_members = TeamMember.order(name: :asc)
+    @team_members = fetch_team_members
     @leaders = @team_members.select { |member| member.is_leader == true }
     @blazers = @team_members.select { |member| member.is_leader == false }
   end
 
   def show
     @team_member = TeamMember.friendly.find(params[:id]).decorate
+  end
+
+  private
+
+  def fetch_team_members
+    TeamMember.includes(:profile_image_attachment).order(name: :asc)
   end
 end
