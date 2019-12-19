406073c793d5418d3736551e0555d8c5d194e9e3
ColinW520 <colinhagan@gmail.com>
2019-12-18 20:58:28 -0700

    bugfix: resolve errantly committed bugfix change
    This is why you don't commit more than one change at a time :facepalm:

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,6 +1,5 @@
 class ParticipantsController < ApplicationController
   before_action :set_event_and_race
-  before_action :set_race
 
   def index
     @participants = @race.participants.for_start_list.decorate
@@ -8,7 +7,7 @@ class ParticipantsController < ApplicationController
 
   private
 
-  def set_event
+  def set_event_and_race
     @event = Event.friendly.find(params[:event_id]).decorate
     @race = @event.races.friendly.find(params[:race_id]).decorate
   end
