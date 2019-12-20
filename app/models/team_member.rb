8c0f5a684863e89efd0d662c6735012038c0b15b
William Blake <gwilliamblake@gmail.com>
2019-12-19 19:13:36 -0600

    parent ca124d4eed6104a6fc06bd1cc51505516a403c84 author William Blake <gwilliamblake@gmail.com> 1576804416 -0600 committer William Blake <gwilliamblake@gmail.com> 1581988359 -0600
    refactors team member card back button
    
    -removes previous/next buttons and logic
    -repositions button and changes button text to "Back to Roster"

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -4,14 +4,6 @@ class TeamMember < ApplicationRecord
   has_one_attached :profile_image
   has_one_attached :cover_image
 
-  def previous
-    TeamMember.where("id < ?", id).order("id DESC").first || TeamMember.last
-  end
-
-  def next
-    TeamMember.where("id > ?", id).order("id ASC").first || TeamMember.first
-  end
-
   extend FriendlyId
   friendly_id :name, use: :slugged
   def should_generate_new_friendly_id?
