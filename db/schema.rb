a30ec0d319f2684da955f6f222ec6e12c35c7819
ColinW520 <colinhagan@gmail.com>
2019-12-20 13:14:38 -0700

    feature: adds a field to store user descriptions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_15_184705) do
+ActiveRecord::Schema.define(version: 2019_12_20_201348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -342,6 +342,7 @@ ActiveRecord::Schema.define(version: 2019_12_15_184705) do
     t.boolean "is_admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "bio_text"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
