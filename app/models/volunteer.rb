6943dd03d6763f625070765d983b1f3d8cc21573
William Blake <gwilliamblake@gmail.com>
2020-10-09 21:52:34 -0500

    Add model associations

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,2 +1,10 @@
 class Volunteer < ApplicationRecord
+  belongs_to :event
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone, presence: true
+  validates :email, presence: true
+  validates :position, presence: true
+  validates :time, presence: true
 end
