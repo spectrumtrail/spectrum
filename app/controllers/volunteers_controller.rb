5f4840cd478a88c61f1c1e34cba9705069eec6c4
William Blake <gwilliamblake@gmail.com>
2020-10-11 12:06:53 -0500

    Add form params to table record

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -5,6 +5,19 @@ class VolunteersController < ApplicationController
   end
 
   def create
-    
+    @volunteer = Volunteer.create(volunteer_params)
+    if @volunteer
+      flash[:success] = 'Thank you for volunteering!'
+      redirect_to root_path
+    else
+      flash[:error] = 'Error saving volunteer registration, please try again.'
+      redirect_to new_volunteer_path
+    end
+  end
+
+  private
+
+  def volunteer_params
+    params.require(:volunteer).permit(:event_id, :first_name, :last_name, :phone, :email, :position, :time)
   end
 end
