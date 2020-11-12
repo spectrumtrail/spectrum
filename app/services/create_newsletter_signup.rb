e1913e3877aa5c62bf1326a18ed5dd850811b94b
William Blake <gwilliamblake@gmail.com>
2020-11-11 21:00:38 -0600

    refactor newsletter signup service

--- app/services/create_newsletter_signup.rb
+++ app/services/create_newsletter_signup.rb
@@ -13,12 +13,20 @@ class CreateNewsletterSignup
   end
 
   def perform
-    signup_api_call
+    sendgrid_api_call
   end
 
   private
 
-  def signup_api_call
+  def catch_exception(exception)
+    ServiceResponse.new(
+      message: exception.message + ' Please try again.',
+      object: exception,
+      success: false
+    )
+  end
+
+  def sendgrid_api_call
     api_call_data = {
       'list_ids' => list_ids,
       'contacts' => [
@@ -29,5 +37,20 @@ class CreateNewsletterSignup
     }
     sg = SendGrid::API.new(api_key: api_key)
     response = sg.client.marketing.contacts.put(request_body: api_call_data)
+    if response.status_code == '202'
+      ServiceResponse.new(
+        message: 'Thank you for signing up!',
+        object: response,
+        success: true
+      )
+    else
+      ServiceResponse.new(
+        message: 'Something went wrong, please try again.',
+        object: response,
+        success: false
+      )
+    end
+  rescue => e
+    catch_exception(e)
   end
 end
