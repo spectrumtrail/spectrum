42422fbe1411605ce951745405ac1389db87c6b3
William Blake <gwilliamblake@gmail.com>
2020-11-19 12:15:55 -0600

    add traits to event factory

--- spec/factories/event.rb
+++ spec/factories/event.rb
@@ -8,5 +8,13 @@ FactoryBot.define do
     time_zone { "Central Time (US & Canada)" }
     registration_opens_at { 1.day.from_now }
     registration_closes_at { 2.months.from_now - 1.day }
+
+    trait :is_active do
+      is_active { true }
+    end
+
+    trait :not_active do
+      is_active { nil }
+    end
   end
 end
