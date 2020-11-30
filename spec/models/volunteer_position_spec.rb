d46839e7da5ccb7e65cd1835cd178a0ba44df74f
William Blake <gwilliamblake@gmail.com>
2020-11-30 13:28:49 -0600

    refactor volunteer position spec

--- spec/models/volunteer_position_spec.rb
+++ spec/models/volunteer_position_spec.rb
@@ -3,8 +3,9 @@ require "rails_helper"
 RSpec.describe VolunteerPosition, type: :model do
   let!(:race) { create :race }
   let!(:event) { race.event }
+  let(:volunteer_position) { create :volunteer_position }
 
-  subject { VolunteerPosition.new(event_id: event.id, name: "Pizza Kitchen") }
+  subject { volunteer_position }
 
   it "is valid with valid attributes" do
     expect(subject).to be_valid
@@ -12,6 +13,12 @@ RSpec.describe VolunteerPosition, type: :model do
 
   it "is not valid without an event id" do
     subject.event_id = nil
+
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a name" do
+    subject.name = nil
 
     expect(subject).to_not be_valid
   end
