9994fd9a257302f7ad4ee6fd70472c3d3b1d3437
William Blake <gwilliamblake@gmail.com>
2020-12-10 13:46:12 -0600

    update models

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,9 +1,10 @@
 class Volunteer < ApplicationRecord
+  belongs_to :event
   belongs_to :volunteer_position
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :phone, presence: true
-  validates :email, presence: true, uniqueness: { message: "We\'re sorry, you can only volunteer for one event at a time." }
+  validates :email, presence: true, uniqueness: { scope: :event }
   validates :time, presence: true
 end
--- app/models/volunteer_position.rb
+++ app/models/volunteer_position.rb
@@ -1,4 +1,6 @@
 class VolunteerPosition < ApplicationRecord
   belongs_to :event
   has_many :volunteers
+
+  validates :name, presence: true, uniqueness: { scope: :event_id }
 end
