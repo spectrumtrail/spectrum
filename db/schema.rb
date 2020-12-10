bc6e078328b6172653698eb4b0cb319b3744f372
William Blake <gwilliamblake@gmail.com>
2020-12-10 13:43:21 -0600

    update schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_021935) do
+ActiveRecord::Schema.define(version: 2020_12_08_155901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -372,6 +372,7 @@ ActiveRecord::Schema.define(version: 2020_11_18_021935) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "volunteer_position_id"
+    t.boolean "active"
     t.index ["event_id"], name: "index_volunteers_on_event_id"
     t.index ["volunteer_position_id"], name: "index_volunteers_on_volunteer_position_id"
   end
