cd05da8224bc72194de7a6270b40e23109f9fea8
William Blake <gwilliamblake@gmail.com>
2021-01-12 15:16:51 -0600

    refactor controller

--- app/controllers/newsletter_signups_controller.rb
+++ app/controllers/newsletter_signups_controller.rb
@@ -1,17 +1,13 @@
 class NewsletterSignupsController < ApplicationController
 
-  NewsletterSignup = Struct.new(:first_name, :last_name, :email, keyword_init: true)
+  NewsletterSignup = Struct.new(:newsletter_signup, keyword_init: true)
 
   def create
-    newsletter_signup = NewsletterSignup.new(
-      {
-        first_name: newsletter_signup_params[:first_name],
-        last_name: newsletter_signup_params[:last_name],
-        email: newsletter_signup_params[:email]
-      }
+    @newsletter_signup = NewsletterSignup.new(
+      newsletter_signup: newsletter_signup_params
     )
 
-    result = CreateNewsletterSignup.new(**newsletter_signup.to_h).perform
+    result = CreateNewsletterSignup.new(newsletter_signup: @newsletter_signup).call
 
     if result.success
       redirect_back(fallback_location: root_path, success: result.flash_message)
@@ -23,6 +19,6 @@ class NewsletterSignupsController < ApplicationController
   private
 
   def newsletter_signup_params
-    params.require(:newsletter_signup).permit(:first_name, :last_name, :email)
+    params.require(:newsletter_signup).permit!
   end
 end
